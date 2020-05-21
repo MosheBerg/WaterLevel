4c61b3b47ecec84f5c91164f157618e27a55a75c
MosheBerg <moshebergwerk@gmail.com>
2020-05-21 15:38:00 +0300

    added nedb

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
-app.listen(3000, () => console.log('hello 3000'));
+app.listen(3000, () => console.log('hello {port}'));
+app.use(express.static('public'));
+
