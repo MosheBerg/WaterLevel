1a1833e9b732e48284382ab5fdc60daa23d02f8e
MosheBerg <moshebergwerk@gmail.com>
2020-05-21 16:45:29 +0300

    fixed bugs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
-app.listen(3000, () => console.log('hello {port}'));
+app.listen(3000, () => console.log('started listening'));
 app.use(express.static('public'));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
--- public/script.js
+++ public/script.js
@@ -1,93 +1,105 @@
 const xs = [];
-        const ys = [];
-        getdata();
-        async function getdata() {
-            let url_path = 'https://data.gov.il/api/action/datastore_search?offset=';
-            let offset = 0;
-            let url_end = '&resource_id=2de7b543-e13d-4e7e-b4c8-56071bc4d3c8';
-            let total = 9015;
-            while (offset < total) {
-                let url = url_path  + offset + url_end;
-                const response = await fetch(url);
-                const json = await response.json();
-                let records = json.result.records;
+const ys = [];
 
-                let avg = 0;
-                let count = 0;
-                records.forEach(element => {                    
-                    avg += element['Kinneret_Level'];
-                    count++;
-                });
-                ys.push(avg/count);
+getdata();
+async function getdata(lineOffset = 0) {
+    let url_path = 'https://data.gov.il/api/action/datastore_search?offset=';
+    let offset = 0;
+    let url_end = '&resource_id=2de7b543-e13d-4e7e-b4c8-56071bc4d3c8';
+    let total = 9015;
+   while (offset < total) {
+        let url = url_path  + offset + url_end;
+        // let url = 'kinneret_level.csv';
+        const response = await fetch(url);
+        const json = await response.json();
+        //.json();
+        //let records = json.split('\n');
+        let records = json.result.records;
 
-                let date = new Date(records[0]['Survey_Date']);
-                xs.push(date.getMonth() + "/" + date.getFullYear());
-                console.log(xs);
-                if (offset == 0)
-                    total = json.result.total;
-                offset += 100;
-                console.log(total + " " + offset);
-            }
-        }
+        let avg = 0;
+        let count = 0;
+        records.forEach(element => {                    
+            avg += element['Kinneret_Level'];
+            count++;
+        });
+        ys.push(avg/count + lineOffset);
+        let date = new Date(records[0]['Survey_Date']);
+        xs.push(date.getMonth() + "/" + date.getFullYear());
+        // console.log(xs);
+        if (offset == 0)
+            total = json.result.total;
+        offset += 100;
+        console.log(total + " " + offset);
+        // records.forEach(elem => {
+        //     let col = elem.split(',');
+        //     let date = col[0].split('/');
+        //     xs.push(date[date.length - 1]);
+        //     ys.push(parseFloat(col[1])); 
+        //     console.log(xs[xs.length - 1], ys[ys.length -1]);
+        // })
+       
+   }
+    createChart();
+}
 
 
-        createChart();
 
-        function createChart() {
-            var ctx = document.getElementById('myChart').getContext('2d');
-            var chart = new Chart(ctx, {
-                // The type of chart we want to create
-                type: 'line',
 
-                // The data for our dataset
-                data: {
-                    labels: xs,
-                    datasets: [{
-                        label: ':מפלס המים',
-                        //backgroundColor: 'rgb(0, 99, 132)',
-                        borderColor: 'rgb(0, 0, 255)',
-                        data: ys,
-                        borderWidth: 1,
-                        fill: false,
-                        showLine: true,
-                        pointRadius: 0
-                    }]
-                },
+function createChart() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
 
-                // Configuration options go here
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                callback: function (value, index, values) {
-                                    return value + ' m';
-                                }
-                            }
-                        }]
-                    },
-                    plugins: {
-                        zoom: {
-                            // Container for pan options
-                            pan: {
-                                // Boolean to enable panning
-                                enabled: true,
+        // The data for our dataset
+        data: {
+            labels: xs,
+            datasets: [{
+                label: ':מפלס המים',
+                //backgroundColor: 'rgb(0, 99, 132)',
+                borderColor: 'rgb(0, 0, 255)',
+                data: ys,
+                borderWidth: 1,
+                fill: false,
+                showLine: true,
+                pointRadius: 0
+            }]
+        },
 
-                                // Panning directions. Remove the appropriate direction to disable 
-                                // Eg. 'y' would only allow panning in the y direction
-                                mode: 'xy'
-                            },
-
-                            // Container for zoom options
-                            zoom: {
-                                // Boolean to enable zooming
-                                enabled: true,
-
-                                // Zooming directions. Remove the appropriate direction to disable 
-                                // Eg. 'y' would only allow zooming in the y direction
-                                mode: 'xy',
-                            }
+        // Configuration options go here
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        callback: function (value, index, values) {
+                            return value + ' m';
                         }
                     }
+                }]
+            },
+            plugins: {
+                zoom: {
+                    // Container for pan options
+                    pan: {
+                        // Boolean to enable panning
+                        enabled: true,
+
+                        // Panning directions. Remove the appropriate direction to disable 
+                        // Eg. 'y' would only allow panning in the y direction
+                        mode: 'xy'
+                    },
+
+                    // Container for zoom options
+                    zoom: {
+                        // Boolean to enable zooming
+                        enabled: true,
+
+                        // Zooming directions. Remove the appropriate direction to disable 
+                        // Eg. 'y' would only allow zooming in the y direction
+                        mode: 'xy',
+                    }
                 }
-            });
+            }
         }
+    });
+}
