f03ee15d8a1b7effbd9b7cfaf2a3f6ef5d649284
MosheBerg <moshebergwerk@gmail.com>
2020-05-21 17:40:43 +0300

    added process port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const Datastore = require('nedb');
-
+const port = process.env.PORT || 3000;
 const app = express();
-app.listen(3000, () => console.log('started listening'));
+app.listen(port, () => console.log('started listening'));
 app.use(express.static('public'));
 
 const database = new Datastore('database.db');
